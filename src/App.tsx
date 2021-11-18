ca2d90e30818354be5bfef636b2c74dee56779fb
lmachens <leon@machens.koeln>
2021-11-18 12:50:31 +0100

    ✨ Set starter code with sample button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,20 @@
 import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from './components/button/Button';
+import styles from './App.module.css';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="app">
-      <header className="app-header">
-        <img src={logo} className="app-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="app-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="app-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className={styles.app}>
+      <p>Hello Vite + React!</p>
+      <p>
+        <Button
+          size="large"
+          onClick={() => setCount((count) => count + 1)}
+          label={`count is: ${count}`}
+        />
+      </p>
     </div>
   );
 }
