5e57bbd04dd531299d9984c6eb5a159aff1b5886
lmachens <leon@machens.koeln>
2021-11-18 16:30:46 +0100

    ✨ Call API from client

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,10 @@ import express from 'express';
 const port = 3001;
 const app = express();
 
+app.get('/api/hello', (_request, response) => {
+  response.json({ message: 'Hello from server' });
+});
+
 app.get('/', (_request, response) => {
   response.send('Server is running');
 });
