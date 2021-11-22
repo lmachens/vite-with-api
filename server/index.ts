2f0304845b4c4020214589fe4941438d6bfc2961
lmachens <leon@machens.koeln>
2021-11-22 10:44:42 +0100

    ✨ Serve app

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 
 const port = 3001;
 const app = express();
@@ -7,8 +8,12 @@ app.get('/api/hello', (_request, response) => {
   response.json({ message: 'Hello from server' });
 });
 
-app.get('/', (_request, response) => {
-  response.send('Server is running');
+// Serve production bundle
+app.use(express.static('dist'));
+
+// Handle client routing, return all requests to the app
+app.get('*', (_request, response) => {
+  response.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 app.listen(port, () => {
